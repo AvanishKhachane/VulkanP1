ec364cbce74ebdc891db180a03c64a5f04ff5694
AvanishKhachane <avanishkhachane19@gmail.com>
2025-09-04 22:37:42 +0530

    Auto Commit

--- proj/veWindow.hpp
+++ proj/veWindow.hpp
@@ -3,7 +3,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
-
 #include <string>
 
 using namespace std;
@@ -13,22 +12,22 @@ namespace ve
 	class veWindow
 	{
 	private:
-
 		const int width, height;
 
 		string name;
-		GLFWwindow* window;
+		GLFWwindow *window;
+
 	public:
 		veWindow(const int width, const int height, const string name);
 
-		veWindow(const veWindow&) = delete;
-		veWindow &operator=(const veWindow&) = delete;
+		veWindow(const veWindow &) = delete;
+		veWindow &operator=(const veWindow &) = delete;
 
-		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
+		void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
 
 		void initWindow();
 
-		VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; };
+		VkExtent2D getExtent() { return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)}; };
 
 		inline bool shouldClose() { return glfwWindowShouldClose(window); };
 
